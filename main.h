5bc46c68b27053ca66eb2987b5ec82386ebe4126
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-04-16 13:47:06 -0700

    corrected main.h

--- main.h
+++ main.h
@@ -9,15 +9,14 @@ int _printf(const char *format, ...);
 int print_char(va_list arg);
 int print_str(va_list arg);
 int print_percent(void);
-void print_binary(unsigned int n, unsigned int* printed);
+void print_binary(unsigned int n, unsigned int *printed);
 int print_unsignedToBinary(va_list arg);
 int print_oct(va_list arg);
 int print_unsignedIntToHex(unsigned int num, char _case);
 int print_hex_base(va_list arg, char _case);
 int print_hex(va_list arg);
 int print_HEX(va_list arg);
-int print_STR (va_list arg);
-
+int print_STR(va_list arg);
 /**
  * struct identifierStruct - structure definition of a printTypeStruct
  * @indentifier: type
