598a644c030696452de50db5b5144dde9d0f3807
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-04-18 07:26:06 -0700

    corrected print_binary.c

--- print_binary.c
+++ print_binary.c
@@ -9,12 +9,12 @@
 
 void print_binary(unsigned int n, unsigned int *printed)
 {
-	int printed=0,rem,f=1;
+	int *printed=0,rem,f=1;
 	while(n != 0){
 		rem = n % 2;
-		printed = printed + rem * f;
+		*printed = *printed + rem * f;
 		f = f * 10;
 		n = n / 2;
 	}
-	return printed;
+	return *printed;
 }
